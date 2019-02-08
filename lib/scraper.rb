d901575c0100f45175889e7b5730b2c673da99f7
Tom Holmes <tholmes59@gmail.com>
2019-02-08 20:03:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,32 +20,19 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-     student = {}
+    profile = {}
     profile_page = Nokogiri::HTML(open(profile_url))
-    # links = profile_page.css(".social-icon-container").children.css("a").map { |el| el.attribute('href').value}
-    # links.each do |link|
-    #   if link.include?("linkedin")
-    #     student[:linkedin] = link
-    #   elsif link.include?("github")
-    #     student[:github] = link
-    #   elsif link.include?("twitter")
-    #     student[:twitter] = link
-    #   else
-    #     student[:blog] = link
-    #   end
-    # end
-    student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value
+   
+    profile[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value
     if profile_page.css(".social-icon-container").children.css("a")[0]
-    student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1]
-    student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2]
-    student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[3]
-    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
-    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-
-    student
+    profile[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1]
+    profile[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2]
+    profile[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[3]
+    profile[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    profile[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+    profile
   end
-
-end
+  end
   
 
 end

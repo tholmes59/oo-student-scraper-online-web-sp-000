104ad8d67bc0c52b8c33259812ca21d0181e754f
Tom Holmes <tholmes59@gmail.com>
2019-02-08 19:58:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,27 +15,27 @@ class Scraper
         }
         students << hash
     end
-    
   end
   students 
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile_page = Nokogiri::HTML(open(profile_url))
+    profiles = {}
+    profile_page.css("").each do |profile|
+        hash = {
+        :twitter => profile.css(".social-icon-container").children.css("a")[0].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[0],
+        :linkedin => profile.css(".social-icon-container").children.css("a")[1].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[1],
+        :github => profile.css(".social-icon-container").children.css("a")[2].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[2],
+        :blog => profile.css(".social-icon-container").children.css("a")[4].attribute("href").value if profile_page.css(".social-icon-container").children.css("a")[4],
+        :profile_quote => profile.css('.vitals-text-container .profile-quote').text if profile.css('.vitals-text-container .profile-quote').text,
+        :bio => profile.css('div.bio-content.content-holder div.description-holder p').text if profile.css('div.bio-content.content-holder div.description-holder p').text
+        }
+        profiles << hash
+    end
   end
+  profiles 
+  end
+  
 
 end
-
-
-# index_page = Nokogiri::HTML(open(index_url))
-#     students = []
-#     index_page.css("div.roster-cards-container").each do |card|
-#       card.css(".student-card a").each do |student|
-#         student_profile_link = "#{student.attr('href')}"
-#         student_location = student.css('.student-location').text
-#         student_name = student.css('.student-name').text
-#         students << {name: student_name, location: student_location, profile_url: student_profile_link}
-#       end
-#     end
-#     students
-#   end
